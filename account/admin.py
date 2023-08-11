92ccd619498371942515a9cff969e458ab6ae852
ThaiTran31 <thaitrancs31@gmail.com>
2023-08-11 10:40:51 +0700

    [ADD] defining models and configuring admin panel for account app

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Profile
 
 
-admin.site.register(CustomUser, UserAdmin)
+class ProfileInline(admin.TabularInline):
+    model = Profile
+    extra = 0
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "name",
+        "age_limit",
+    ]
+    list_filter = [
+        "age_limit",
+    ]
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    inlines = [
+        *UserAdmin.inlines,
+        ProfileInline,
+    ]
--- account/models.py
+++ account/models.py
@@ -1,6 +1,28 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Profile(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=100)
+    AGE_CHOICES = [
+        ("all", "All"),
+        ("kids", "Kids"),
+    ]
+    age_limit = models.CharField(
+        choices=AGE_CHOICES,
+        max_length=10,
+    )
